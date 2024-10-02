f6de24df8a2d4e2cb58a3d6cdb3c33bbf336b459
Leavone <65247238+Leavone@users.noreply.github.com>
2024-10-02 23:10:35 +0400

    Neater code

--- FS3/hacker-rm.cpp
+++ FS3/hacker-rm.cpp
@@ -3,35 +3,44 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
 #define BUFF_SIZE 4096
 
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
 
-	 if(argc == 1){
-                perror("File destination has not been passed");
-                return 1;
-        }
+    // Return if no argument has been passed
+    if (argc == 1) {
+        perror("File destination has not been passed");
+        return 1;
+    }
 
-	int fd = open(argv[1], O_RDWR);
-	
-	if (fd == -1){
-		perror("Couldn't open the file");
-		exit(EXIT_FAILURE);
-	}
+    int fd = open(argv[1], O_RDWR);
 
-	char buffer[BUFF_SIZE] = {'\0'};
-	int file_size = lseek(fd, 0, SEEK_END);
-	lseek(fd, 0, SEEK_SET);
+    if (fd == -1) {
+        perror("Couldn't open the file");
+        exit(EXIT_FAILURE);
+    }
 
-	while(file_size > BUFF_SIZE){
-		write(fd, buffer, BUFF_SIZE);
-		file_size -= BUFF_SIZE;
-		std::cout<<file_size<<'\n';
-	}
-	write(fd, buffer, file_size);
-	close(fd);
-	unlink(argv[1]);
-	return 0;
+    // Initialize null buffer of defined size
+    char buffer[BUFF_SIZE] = {'\0'};
+
+    // Get the number of bytes in the file (it sends cursor to the end)
+    int file_size = lseek(fd, 0, SEEK_END);
+
+    // Return cursor to the begining
+    lseek(fd, 0, SEEK_SET);
+
+    // Write buffer into the file
+    while (file_size > BUFF_SIZE) {
+        write(fd, buffer, BUFF_SIZE);
+        file_size -= BUFF_SIZE;
+        std::cout << file_size << '\n';
+    }
+    write(fd, buffer, file_size);
+
+    close(fd);
+
+    // Unlinking passed filename from the data
+    unlink(argv[1]);
+    return 0;
 }
