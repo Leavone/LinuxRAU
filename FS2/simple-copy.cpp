1045394108d3de0f2c09bbe71625dc01d13c2066
Leavone <65247238+Leavone@users.noreply.github.com>
2024-10-02 23:17:58 +0400

    Neater code

--- FS2/simple-copy.cpp
+++ FS2/simple-copy.cpp
@@ -3,27 +3,39 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char **argv){
-	int fd = open(argv[1], O_RDONLY);
-	if(fd == -1){
-		perror("Couldn't load the file");
-		return 1;
-	}
-	int dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if(dest == -1){
-		perror("Couldn't load or create destination file");
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char **argv) {
 
-	char buffer[100];
-	while(true){
-		ssize_t bytes_read = read(fd, buffer, sizeof(buffer) - 1);
-		if(bytes_read == 0)
-			break;
-		write(dest, buffer, bytes_read);
-	}
-	close(fd);
-	close(dest);
-	return 0;
+    if (argc != 3) {
+        perror("Not all arguments have been passed");
+        return 1;
+    }
+
+    int source = open(argv[1], O_RDONLY);
+    if (source == -1) {
+        perror("Couldn't load the source file");
+        return 1;
+    }
+    
+    // Opening destination file
+    int destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (destination == -1) {
+        perror("Couldn't load or create destination file");
+        exit(EXIT_FAILURE);
+    }
+    
+    char buffer[100];
+    
+    // Reading from source until bytes_read is 0 and writing into destination
+    while (true) {
+        ssize_t bytes_read = read(source, buffer, sizeof(buffer) - 1);
+        if (bytes_read == 0)
+            break;
+        write(destination, buffer, bytes_read);
+    }
+    
+    close(source);
+    close(destination);
+    
+    return 0;
 }
 
