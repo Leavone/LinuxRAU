f90801e9a52865f51f83f21444b9167719fe411d
Leavone <65247238+Leavone@users.noreply.github.com>
2024-10-02 23:20:23 +0400

    Neater code

--- FS1/read-file.cpp
+++ FS1/read-file.cpp
@@ -2,20 +2,30 @@
 #include <iostream>
 #include <unistd.h>
 
-int main(int argc, char **argv){
-	char buffer[100];
-	int fd = open(argv[1], O_RDONLY);
-	if(fd == -1){
-		std::cout<<"Couldn't read the file\n";
-		exit(EXIT_FAILURE);
-	}
-	while(true){
-		ssize_t bytes_read = read(fd, buffer, sizeof(buffer) - 1);
-		if(bytes_read == 0)
-			break;
-		buffer[bytes_read] = '\0';
-		std::cout<<buffer;
-	}
-	close(fd);
-	return 0;
+int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        perror("Not all arguments have been passed");
+        return 1;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        std::cout << "Couldn't read the file\n";
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[100];
+
+    while (true) {
+        ssize_t bytes_read = read(fd, buffer, sizeof(buffer) - 1);
+        if (bytes_read == 0)
+            break;
+        buffer[bytes_read] = '\0';
+        std::cout << buffer;
+    }
+
+    close(fd);
+    
+    return 0;
 }
